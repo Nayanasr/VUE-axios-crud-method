4f35f9cc59b3b2affae1d67959b833c8020c0141
Nayana Ramesh <107012@tfcorp.com>
2022-10-04 11:29:37 +0530

    products app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+  <!-- <products-main /> -->
+  <!-- <user-comp /> -->
+  <main-assign />
+
+
+  </div>
+</template>
+
+<script>
+import MainAssign from './components/Main-assign.vue'
+// import UserComp from './components/UserComp.vue'
+// import ProductsMain from './components/Products-main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainAssign
+    // UserComp
+  //  ProductsMain
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/UserComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>User</h1>
+</template>
+
+<script>
+// import axios from "axios";
+import { JSONUSERS } from "../axiosConfig";
+export default {
+  methods: {
+    async fetchUsers() {
+      let res = await JSONUSERS.get("/users");
+      console.log("user res", res);
+    },
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+};
+</script>
+
+<style></style>
